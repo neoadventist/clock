fda1738c359971058f1013d6bdb63d6636a2c29e
neoadventist <git@spencersims.com>
2013-08-09 18:14:16 -0700

    Added Minute Control Logic. Removed Yellow Class (No Class default). Hour Function not confirmed tested

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = angular.module('CLOCK', ['ui.bootstrap']).
 
 app.directive('time', function() {
     return {
-		scope: {
+	scope: {
             text: '@text',
             glow: '=',
 			match: '=',
@@ -20,12 +20,50 @@ app.directive('time', function() {
         replace: true,
 		restrict: 'A',
 		link: function(scope, element, attrs) {
-			scope.$watch('minute', function(newValue, oldValue) {		
-				if(scope.glow==scope.hour || scope.glow==scope.minute){
+			scope.$watch('minute', function(newValue, oldValue) {
+				var h = scope.hour;
+				var n = scope.minute;
+				var g = scope.glow;
+				//scope.glowClass="yellow";
+				switch (true){
+					case (n>=0 && n<5) && (g=="h"):
 					scope.glowClass="red";
-					console.log(attrs.glow+" "+attrs.hour+" "+attrs.minute);
-				}else{
-					scope.glowClass="yellow";
+					break;
+					case (n>=5 && n<9) && (g=="f" || g=="p"):
+					scope.glowClass="red";
+					break;
+					case (n>=9 && n<=12) && (g=="ten" || g=="p"):
+					scope.glowClass="red";
+					break;
+					case (n>=13 && n<17) && (g=="a" || g=="p"):
+					scope.glowClass="red";
+					break;
+					case (n>=17 && n<25) && (g=="20" || g=="p"):
+					scope.glowClass="red";
+					break;
+					case (n>=25 && n<31) && (g=="30" || g=="p"):
+					scope.glowClass="red";
+					break;
+					case (n>=31 && n<35) && (g=="30" || g=="t"):
+					scope.glowClass="red";h++;
+					break;
+					case (n>=35 && n<41) && (g=="20" || g=="m" || g=="t"):
+					scope.glowClass="red";h++;
+					break;
+					case (n>=41 && n<49) && (g=="a" || g=="t"):
+					scope.glowClass="red";h++;
+					break;
+					case (n>=50 && n<55) && (g=="ten" || g=="m" || g=="t"):
+					scope.glowClass="red";h++;
+					break;
+					case (n>=55 && n<=60) && (g=="f" || g=="m" || g=="t"):
+					scope.glowClass="red";h++;
+					break;
+				}
+				
+				if(scope.glow==h || scope.text=="It Is" ){
+					scope.glowClass="red";
+					//console.log(attrs.glow+" "+attrs.hour+" "+attrs.minute);
 				}
 			}, true);
 		},
--- controllers.js
+++ controllers.js
@@ -4,7 +4,17 @@ app.controller('main', function ($scope, $http,$location, $filter) {
 	$scope.hour=$filter('date')(new Date(), 'h');
 	$scope.minute=$filter('date')(new Date(), 'm');
 	
-	if(Number($scope.minute)<45 && Number($scope.minute)>25){
+/*
+	var n = Number($scope.minute); 
+	
+	if(n<45 && n>25){
 		$scope.minute=30;
 	}
+	if(n>51 && n<59){
+		$scope.minute=55; 
+	}
+	if(n==60 || n==0){
+		$scope.h=true;
+	}
+*/
 });
